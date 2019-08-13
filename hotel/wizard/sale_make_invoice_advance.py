f36c21080f3666d75bcec603d02a3c258100f9dc
Ranco35 <44340061+Ranco35@users.noreply.github.com>
2019-08-13 16:30:06 -0400

    up

--- hotel/wizard/sale_make_invoice_advance.py
+++ hotel/wizard/sale_make_invoice_advance.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
@@ -8,37 +9,39 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
     @api.model
     def _get_advance_payment(self):
-        ctx = self.env.context.copy()
+        ctx = self._context.copy()
         if self._context.get('active_model') == 'hotel.folio':
             hotel_fol = self.env['hotel.folio']
-            hotel = hotel_fol.browse(self._context.get('active_ids',
-                                                       []))
+            hotel = hotel_fol.browse(self._context.get('active_ids', []))
             ctx.update({'active_ids': [hotel.order_id.id],
                         'active_id': hotel.order_id.id})
-        return super(SaleAdvancePaymentInv,
-                     self.with_context(ctx))._get_advance_payment_method()
-    advance_payment_method = fields.Selection(
-        [
-            ('delivered', 'Invoiceable lines'),
-            ('all', 'Invoiceable lines (deduct down payments)'),
-            ('percentage', 'Down payment (percentage)'),
-            ('fixed', 'Down payment (fixed amount)')
-        ], string='What do you want to invoice?',
-        default=_get_advance_payment,
-        required=True
-    )
+        return super(SaleAdvancePaymentInv, self.with_context(ctx)). \
+            _get_advance_payment_method()
+
+    advance_payment_method = fields.Selection([('delivered',
+                                                'Invoiceable lines'),
+                                               ('all',
+                                                'Invoiceable lines '
+                                                '(deduct down payments)'),
+                                               ('percentage',
+                                                'Down payment (percentage)'),
+                                               ('fixed',
+                                                'Down payment (fixed '
+                                                'amount)')],
+                                              string='What do you want '
+                                                     'to invoice?',
+                                              default=_get_advance_payment,
+                                              required=True)
 
     @api.multi
     def create_invoices(self):
-        ctx = self.env.context.copy()
+        ctx = self._context.copy()
         if self._context.get('active_model') == 'hotel.folio':
             hotel_fol = self.env['hotel.folio']
-            hotel = hotel_fol.browse(self._context.get('active_ids',
-                                                       []))
+            hotel = hotel_fol.browse(self._context.get('active_ids', []))
             ctx.update({'active_ids': [hotel.order_id.id],
                         'active_id': hotel.order_id.id,
                         'folio_id': hotel.id})
         res = super(SaleAdvancePaymentInv,
                     self.with_context(ctx)).create_invoices()
-
         return res
